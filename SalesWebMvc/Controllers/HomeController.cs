93211a72315cc85b91f2b3a21130cf5133a59cb6
Edson Maia <Edson.Maia@fadami.com.br>
2022-08-30 11:23:06 -0300

    Controller test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aplicação teste Razor Pages.";
+            ViewData["Developer"] = "Edson Moura";
 
             return View();
         }
